ac405f27abea6b24b8d5bada92783666b8273581
Eric Siebeneich <eric.siebeneich@gmail.com>
2015-07-15 22:15:52 -0500

    Base assets and PlatformMover/Destroyer

---
+++ Assets/Scripts/PlatformMover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformMover : MonoBehaviour {
+
+    public static float PlatformSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.left * Time.deltaTime * PlatformSpeed);
+	}
+}
